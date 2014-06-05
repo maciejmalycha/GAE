68a72acd8a15828b908519663e98d2ee8a96baa6
Maciej Malycha <maciej.malycha@genesyslab.com>
2014-06-05 08:30:15 +0200

    Basic handler

---
+++ src/appengine.py
@@ -0,0 +1,17 @@
+import webapp2
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
